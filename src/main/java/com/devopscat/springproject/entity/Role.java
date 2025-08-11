329f85d6ecf95792c479babf9ca5ad63cc070a4b
jiwon <asdfoqlk@naver.com>
2025-08-11 11:08:31 +0900

    user bean 파일 생성

---
+++ src/main/java/com/devopscat/springproject/entity/Role.java
@@ -0,0 +1,6 @@
+package com.devopscat.springproject.entity;
+
+public enum Role {
+    // enum 상수들의 집합
+    ADMIN, MANAGER, USER;
+}
---
+++ src/main/java/com/devopscat/springproject/entity/User.java
@@ -0,0 +1,9 @@
+package com.devopscat.springproject.entity;
+
+public class User {
+    private int idx;
+    private String username;
+    private String password;
+    private String writer;
+    private Role role;
+}
